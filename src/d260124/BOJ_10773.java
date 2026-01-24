1d5ec2331825580b4f48519bdc119fc0d81b1348
jongse7 <mmssjj72@naver.com>
2026-01-24 17:58:30 +0900

    d260124

---
+++ src/d260124/BOJ_10773.java
@@ -0,0 +1,20 @@
+package d260124;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < N; i++){
+            int n = Integer.parseInt(br.readLine());
+            if(n == 0) stack.pop();
+            else stack.push(n);
+        }
+        int sum = 0;
+        for(int child : stack)sum+=child;
+        System.out.println(sum);
+    }
+}
