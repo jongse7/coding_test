a047605d220df6a583a06b6d8c0ab0fe35d8e5bc
jongse7 <mmssjj72@naver.com>
2026-01-28 23:28:40 +0900

    d260128

--- src/d260128/BOJ_11866.java
+++ src/d260128/BOJ_11866.java
@@ -1,7 +1,13 @@
-import java.util.*;
-import java.io.*;
+package d260128;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_11866 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
