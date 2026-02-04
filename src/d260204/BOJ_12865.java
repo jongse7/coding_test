fdd078c7045a59b94c9a62932771f6f23f546b06
jongse7 <mmssjj72@naver.com>
2026-02-04 23:22:35 +0900

    d260204

---
+++ src/d260204/BOJ_12865.java
@@ -0,0 +1,40 @@
+package d260204;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_12865 {
+    static int N;
+    static int K;
+    static int[][] items;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dp = new int[N + 1][K + 1];
+        items = new int[N + 1][2];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            items[i][0] = Integer.parseInt(st.nextToken());
+            items[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i <= N; i++) {
+            for(int j = 1; j <= K; j++){
+                if (j - items[i][0] >= 0) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - items[i][0]] + items[i][1]);
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        System.out.print(dp[N][K]);
+    }
+}
