643f19dd9bd672b82f3515ad2d841a7bfe8b138a
jongse7 <mmssjj72@naver.com>
2026-03-16 10:21:02 +0900

    d260316

---
+++ src/d260316/BOJ_10431.java
@@ -0,0 +1,39 @@
+package d260316;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ_10431 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int P = Integer.parseInt(br.readLine());
+        for (int i = 0; i < P; i++) {
+            st = new StringTokenizer(br.readLine());
+            int T = Integer.parseInt(st.nextToken());
+            ArrayList<Integer> students = new ArrayList<>();
+            for (int j = 0; j < 20; j++) students.add(Integer.parseInt(st.nextToken()));
+
+            int count = 0;
+            for (int j = 0; j < 20; j++) {
+                int now = students.get(j);
+                for (int k = 0; k < j; k++) {
+                    if (now < students.get(k)) {
+                        students.remove(j);
+                        students.add(k, now);
+                        count += j - k;
+                        break;
+                    }
+                }
+            }
+
+            sb.append(T).append(" ").append(count).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
