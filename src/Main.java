85c85c2ae320b6471fdf467cc30b7356ffb72a22
jongse7 <mmssjj72@naver.com>
2026-04-10 10:38:48 +0900

    d260410

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,4 @@
-import java.util.*;
-import java.io.*;
 
 public class Main {
-    static public void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        LinkedList<Integer> dq = new LinkedList<>();
-        for(int i = 0; i < N; i++) dq.add(Integer.parseInt(st.nextToken()));
 
-        int next = dq.poll() - 1;
-        sb.append(1).append(" ");
-        while(!dq.isEmpty()){
-            if(next < 0){
-                next = dq.size() + next;
-                System.out.println(next);
-            }else if(next >= dq.size()){
-                next = next - dq.size() - 1;
-            }
-            int now = dq.get(next);
-            System.out.println(now);
-
-            sb.append(next).append(" ");
-            dq.remove(next);
-            next += now;
-        }
-        System.out.print(sb);
-    }
 }
---
+++ src/_2604/d10/BOJ_4999.java
@@ -0,0 +1,20 @@
+package _2604.d10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_4999 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String jaehwan = br.readLine();
+        String doctor = br.readLine();
+
+        if (jaehwan.length() >= doctor.length()) {
+            System.out.println("go");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
